7e330abd02b931a1258f98621389d54aaf8dace0
flxemt <kudavidss@gmail.com>
2023-02-17 16:36:42 +0400

    make it pixel-perfect

--- main.js
+++ main.js
@@ -2,12 +2,13 @@ const showcaseItems = document.querySelectorAll('#showcase > img')
 const prevBtn = document.getElementById('prev-btn')
 const nextBtn = document.getElementById('next-btn')
 
+const gap = -32
 const minPos = -2
 const maxPos = 2
 let currentPos = 0
 
 function calculateTransform() {
-  const transform = `translateX(calc(${-100 * currentPos}% + ${-16 * currentPos}px))`
+  const transform = `translateX(calc(${-100 * currentPos}% + ${gap * currentPos}px))`
 
   showcaseItems.forEach(item => {
     item.style.transform = transform
